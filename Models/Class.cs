235d462b99aab21b56e9459eb9fb1d9b6ece1030
Chrissx77 <garozzochristian@gmail.com>
2024-05-19 19:51:55 +0200

    Add pizzeObjects

---
+++ Models/Class.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication1.Models
+{
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PhotoUrl { get; set; }
+        public decimal Price { get; set;}
+
+        public Pizza(string Name, string Description, string PhotoUrl, decimal Price)
+        {
+            this.Name = Name;
+            this.Description = Description;
+            this.PhotoUrl = PhotoUrl;
+            this.Price = Price;
+        }
+    }
+}
